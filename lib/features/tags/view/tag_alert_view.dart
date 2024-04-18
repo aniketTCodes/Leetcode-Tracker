7f29bd9c50b2bbbb7c9ad1630ebb80bfb66ceec9
Aniket tiwari <ani962854.at@gmail.com>
2024-04-18 06:25:04 +0530

    Tagmodel and color picker ui added

---
+++ lib/features/tags/view/tag_alert_view.dart
@@ -0,0 +1,144 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:leetcode_tracker/core/constants/app_colors.dart';
+import 'package:leetcode_tracker/di/di.dart';
+import 'package:leetcode_tracker/features/tags/bloc/bloc/tags_bloc.dart';
+import 'package:leetcode_tracker/features/tags/data/model/tag_model.dart';
+import 'package:leetcode_tracker/features/tags/data/repository/tag_repository.dart';
+
+class TagAlertDialogueView extends StatefulWidget {
+  final List<TagModel> solutionTags;
+  const TagAlertDialogueView({super.key,required this.solutionTags});
+
+  @override
+  State<TagAlertDialogueView> createState() => _TagAlertDialogueViewState();
+}
+
+class _TagAlertDialogueViewState extends State<TagAlertDialogueView> {
+  Color selectedColor = Colors.white;
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          TagsBloc(repository: getIt<TagRepository>())..add(LoadUserTagEvent()),
+      child: BlocConsumer<TagsBloc, TagsState>(
+        builder: (context, state) {
+          if (state is TagDoneState) {
+            return Container(
+              width: 400,
+              constraints: const BoxConstraints(minHeight: 100),
+              padding: const EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const TextField(
+                      cursorColor: appYellow,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        hintText: 'Enter Tag Name',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        focusColor: appYellow,
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: appYellow,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: double.maxFinite,
+                      height: 40,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => Container(
+                          margin: const EdgeInsets.only(left: 2, right: 2),
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: tagColors[index],
+                              ),
+                            ),
+                            child: ColorIndicator(
+                              borderRadius: 50,
+                              color: tagColors[index],
+                              isSelected: selectedColor == tagColors[index],
+                              onSelect: () {
+                                setState(() {
+                                  selectedColor = tagColors[index];
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        itemCount: tagColors.length,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: double.maxFinite,
+                      child: FloatingActionButton(
+                        backgroundColor: appYellow,
+                        onPressed: () {},
+                        child: const Text('Add'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Your Tags',
+                      style: TextStyle(
+                        color: appYellow,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                      child: Builder(
+                        builder: (context) {
+                          return const Text(
+                            'No Tags found',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+          return const SizedBox(
+            height: 100,
+            child: Center(
+              child: CircularProgressIndicator(
+                color: appYellow,
+              ),
+            ),
+          );
+        },
+        listener: (context, state) {},
+      ),
+    );
+  }
+}
